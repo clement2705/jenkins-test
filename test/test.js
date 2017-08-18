b8d9532ae8b2f6e5298dc8963904cdabead0da19
Clement Gohlke <Clement@Clements-MacBook-Pro.local>
2017-08-18 13:47:18 +0800

    test 5 any branch jenkins

--- test/test.js
+++ test/test.js
@@ -17,6 +17,12 @@ describe("mocha tests", function () {
     done();
   });
 
+  it("merged from any branch initial commit", function (done) {
+    //expect(response.statusCode).toBe(200);
+    assert.equal(0, 0);
+    done();
+  });
+
   it("merged from any branch", function (done) {
     //expect(response.statusCode).toBe(200);
     assert.equal(0, 0);
