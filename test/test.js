a9c9b95286a80de208f23b8d956579d5fe5b3f58
Clement Gohlke <Clement@Clements-MacBook-Pro.local>
2017-08-30 11:15:14 +0800

    initial

--- test/test.js
+++ test/test.js
@@ -5,31 +5,8 @@ const url = "http://localhost:" + "8090" + "/graph";
 
 describe("mocha tests", function () {
   console.log("url is", url)
-  it("return true", function (done) {
-    //expect(response.statusCode).toBe(200);
-    assert.equal(0, 0);
-    done();
-  });
 
-  it("merged to master?", function (done) {
-    //expect(response.statusCode).toBe(200);
-    assert.equal(0, 0);
-    done();
-  });
-
-  it("merged from any branch initial commit", function (done) {
-    //expect(response.statusCode).toBe(200);
-    assert.equal(0, 0);
-    done();
-  });
-
-  it("merged from branch test5", function (done) {
-    //expect(response.statusCode).toBe(200);
-    assert.equal(0, 0);
-    done();
-  });
-
-  it("master test Jenkinsfile", function (done) {
+  it("test", function (done) {
     //expect(response.statusCode).toBe(200);
     assert.equal(0, 0);
     done();
